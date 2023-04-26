e436d24cb6c701489e9a384bfbbae68609008ff1
mfernandez <mfernandez@W10N-I2E31.ateis.local>
2023-04-26 18:53:16 +0200

    A6.3

--- js/register_validation.js
+++ js/register_validation.js
@@ -15,12 +15,11 @@ La segunda parte crea la expresión de función cuya ejecución es inmediata (),
 (function () {
 
 
-
     var email = null;
     var pwd1 = null;
     var pwd2 = null;
 
-    let pwdRegExp = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\$@\.\?])(?=.{6,})');
+    let pwdRegExp = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\$@\.\?])(?=.{6,})$');
 
     window.onload = function () {
         onceLoaded();
